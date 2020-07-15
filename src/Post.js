ec342e62e4455dae38a6f1df515d2d1f0b64140d
Soloman Garcia <soloman.garcia@gmail.com>
2020-07-15 12:33:10 -0600

    create Post.css

--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./Post.css";
 
 function Post() {
   return (
-    <div>
+    <div className="post">
       <h3>Username</h3>
       <img
+        className="post__image"
         src="https://www.freecodecamp.org/news/content/images/size/w600/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
         alt="post"
       />
