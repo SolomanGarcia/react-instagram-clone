d80f85edb74a59c7380b71fbc8f3e55593f178b6
Soloman Garcia <soloman.garcia@gmail.com>
2020-07-15 12:44:59 -0600

    work on Post component

--- src/Post.js
+++ src/Post.js
@@ -1,16 +1,27 @@
 import React from "react";
 import "./Post.css";
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
   return (
     <div className="post">
-      <h3>Username</h3>
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt="Soloman"
+          src="/static/images/avatar/1.jpg"
+        />
+        <h3>Username</h3>
+      </div>
+
       <img
         className="post__image"
         src="https://www.freecodecamp.org/news/content/images/size/w600/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
         alt="post"
       />
-      <h4>Username: caption</h4>
+      <h4 className="post__text">
+        <strong>Soloman: </strong>Getting started with REACT
+      </h4>
     </div>
   );
 }
