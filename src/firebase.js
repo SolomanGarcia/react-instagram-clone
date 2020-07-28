3aa0058a9206d972471a214f587bcc7da6ae1dfd
Soloman Garcia <soloman.garcia@gmail.com>
2020-07-28 11:05:35 -0600

    connect firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseapp = firebase.initialzeApp({
+  apiKey: "AIzaSyCiugG-HtypbjCkI-7O3lXdtO66ErVH_Hc",
+  authDomain: "instagram-clone-5bc52.firebaseapp.com",
+  databaseURL: "https://instagram-clone-5bc52.firebaseio.com",
+  projectId: "instagram-clone-5bc52",
+  storageBucket: "instagram-clone-5bc52.appspot.com",
+  messagingSenderId: "165866915483",
+  appId: "1:165866915483:web:3b06c0140cbc7c863de83e"
+});
+
+const db = firebaseapp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
