ce2b4eed05d4491fa25c530362b73606f24133ea
Soloman Garcia <soloman.garcia@gmail.com>
2020-07-30 10:36:41 -0600

    work on modal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,39 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
 import { db } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button } from "@material-ui/core";
+
+// Modal styling
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3)
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
   // State
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   // useEffect runs a piece of code based on a specific condition
 
@@ -22,8 +51,15 @@ function App() {
     });
   }, []);
 
+  const signUp = (event) => {};
+
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -31,6 +67,8 @@ function App() {
           alt="logo"
         />
       </div>
+
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
 
       <h1>Hello World!</h1>
 
