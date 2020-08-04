3167398ad06a3c35e1af1c0759f7d1db1c39e9ae
Soloman Garcia <soloman.garcia@gmail.com>
2020-08-04 11:02:23 -0600

    work on upload functionality

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Button } from "@material-ui/core";
+import firebase from "firebase";
 import { storage, db } from "./firebase";
 
-function ImageUpload() {
+function ImageUpload({ username }) {
   const [image, setImage] = useState(null);
   const [progress, setProgress] = useState(0);
   const [caption, setCaption] = useState("");
@@ -16,13 +17,37 @@ function ImageUpload() {
   const handleUpload = () => {
     const uploadTask = storage.ref(`images/${image.name}`).put(image);
 
-    uploadTask.on("state_changed", (snapshot) => {
-      // Progress function
-      const progress = Math.round(
-        (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-      );
-      setProgress(progress);
-    });
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Progress function
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (error) => {
+        // Error function
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        // Complete function
+        storage
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            // Post image inside db
+            db.collection("posts").add({
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+              caption: caption,
+              imageUrl: url,
+              username: username
+            });
+          });
+      }
+    );
   };
 
   return (
