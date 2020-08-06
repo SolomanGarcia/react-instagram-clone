1ad71c6e169ca567da71734f2a407f90c252074d
Soloman Garcia <soloman.garcia@gmail.com>
2020-08-06 10:42:13 -0600

    set avatar to upper case

--- src/Post.js
+++ src/Post.js
@@ -42,7 +42,7 @@ function Post({ postId, user, username, caption, imageUrl }) {
       <div className="post__header">
         <Avatar
           className="post__avatar"
-          alt={username}
+          alt={username.toUpperCase()}
           src="/static/images/avatar/1.jpg"
         />
         <h3>{username}</h3>
