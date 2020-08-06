091ba3c99e1255b375fd3e36777e3da6f23e6c0b
Soloman Garcia <soloman.garcia@gmail.com>
2020-08-06 09:38:50 -0600

    debug

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Post.css";
 import Avatar from "@material-ui/core/Avatar";
 import { db } from "./firebase";
